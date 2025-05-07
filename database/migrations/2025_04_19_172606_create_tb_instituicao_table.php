ac7bd2d6c5cba71efaf420a9cdf26561e83010c8
Rian Alencar <rian.crash@gmail.com>
2025-05-07 00:24:54 -0300

    Adição de campos novos na migration e Model da instituição.

--- database/migrations/2025_04_19_172606_create_tb_instituicao_table.php
+++ database/migrations/2025_04_19_172606_create_tb_instituicao_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('tb_instituicao', function (Blueprint $table) {
             $table->id();
+            $table->string('nome_representante', 100);
+            $table->string('telefone', 25);
+            $table->string('documentos_representante', 25);
             $table->string('cnpj_instituicao', 18);
             $table->boolean('verificado_instituicao');
             $table->string('logradouro_instituicao');

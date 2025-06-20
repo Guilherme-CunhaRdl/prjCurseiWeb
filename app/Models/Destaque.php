0ab4fa9e09126bbd967db49de4473e7314bdbb49
victor barreto <victor.barreto.contato@gmail.com>
2025-06-20 14:44:44 -0300

    Fiz umas mudanças bruscas na logíca do destaques, mas agora eu acho que realmente tá feito agora.

--- app/Models/Destaque.php
+++ app/Models/Destaque.php
@@ -10,28 +10,30 @@ class Destaque extends Model
     use HasFactory;
 
     protected $table = 'tb_destaque';
-    protected $primaryKey = 'id'; // Garanta que está correto
+    protected $primaryKey = 'id';
     public $incrementing = true;
     public $timestamps = true;
 
     protected $fillable = [
-        'id', // Adicione esta linha
         'id_user',
         'data_destaque',
-        'id_story',
         'foto_destaque',
         'status_destaque'
     ];
 
-    // Relacionamentos
     public function user()
     {
         return $this->belongsTo(User::class, 'id_user');
     }
 
-    public function story()
+    // Relacionamento muitos-para-muitos corrigido
+    public function stories()
     {
-        return $this->belongsTo(Story::class, 'id_story');
+        return $this->belongsToMany(
+            Story::class, 
+            'destaque_story',  // Nome da tabela pivot
+            'destaque_id',     // FK na pivot para destaques
+            'story_id'         // FK na pivot para stories
+        );
     }
-
 }
--- routes/api.php
+++ routes/api.php
@@ -126,5 +126,6 @@ Route::post('/stories/upload', [StoryController::class, 'upload']);
 
     // Listar destaques de um usuário específico
         Route::get('/destaques/{id_user}', [DestaqueController::class, 'index']);
-        Route::post('/destaques/{id_user}/{id_story}', [DestaqueController::class, 'store']);
+        Route::post('/destaques/{id_user}', [DestaqueController::class, 'store']);
         Route::delete('/destaques/{id_user}/{id}', [DestaqueController::class, 'destroy']);
+        Route::post('/destaques/{id_destaque}/add-stories', [DestaqueController::class, 'addStories']);

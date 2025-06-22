997a23ca7bba7bdde7de27dd849f9f84c4d5e5df
victor barreto <victor.barreto.contato@gmail.com>
2025-06-22 15:08:59 -0300

    update funcionando.

--- routes/api.php
+++ routes/api.php
@@ -132,6 +132,6 @@ Route::post('/stories/upload', [StoryController::class, 'upload']);
         Route::get('/destaques/{id_user}', [DestaqueController::class, 'index']);
         Route::post('/destaques/{id_user}', [DestaqueController::class, 'store']);
         Route::delete('/destaques/{id_user}/{id}', [DestaqueController::class, 'destroy']);
-        Route::put('/destaques/{id_destaque}/stories', [DestaqueController::class, 'atualizarDestaques']);
+        Route::post('/destaques/{id_destaque}/stories', [DestaqueController::class, 'atualizarDestaques']);
         Route::get('/destaqueEspecifico/{id}', [DestaqueController::class, 'show']);
         Route::get('/stories/{id_user}', [DestaqueController::class, 'listStories']);
